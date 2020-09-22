6bdb83070f85a57d0778a3c27eb84bd9c77c609c
Mounir Afifi <antontheog@gmail.com>
2020-09-22 13:21:49 +0200

    update

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-sum = function() {
-
-    console.log("sum");
-
-    var number1 = document.getElementById("number1").value;
-    var number2 = document.getElementById("number2").value;
-    //console.log(number1 + number2);
-    document.getElementById("result").innerHTML = parseInt(number1) + parseInt(number2);
-
-};
-
-sub = function() {
-
-    console.log("sub");
-
-    var number1 = document.getElementById("number1").value;
-    var number2 = document.getElementById("number2").value;
-    //console.log(number1 + number2);
-    document.getElementById("result").innerHTML = parseInt(number1) - parseInt(number2);
-
-};
-
-mult = function() {
-
-    console.log("mult");
-
-    var number1 = document.getElementById("number1").value;
-    var number2 = document.getElementById("number2").value;
-    //console.log(number1 + number2);
-    document.getElementById("result").innerHTML = parseInt(number1) * parseInt(number2);
-
-};
-
-div = function() {
-
-    console.log("div");
-
-    var number1 = document.getElementById("number1").value;
-    var number2 = document.getElementById("number2").value;
-    //console.log(number1 + number2);
-
-    if(parseInt(number2) != 0){
-        document.getElementById("result").innerHTML = parseInt(number1) / parseInt(number2);
-
-    }
-    else{
-        document.getElementById("result").innerHTML = "ungueltige Zahlen"
+document.addEventListener('DOMContentLoaded', (event) => {
+    var value_pointer = "value1";
+    var operator ;
+    var buttons= document.getElementsByClassName("btn");
+    for(var i = 0; i<buttons.length; i++){
+        buttons[i].addEventListener("click", getValue)
     }
 
-};
+
+    function getValue(e){
+        var raw_value = e.target.innerText;
+        if (raw_value == "*" || raw_value == "+") {
+            operator = raw_value;
+            document.getElementById("operator").innerText += raw_value;
+            value_pointer = "value2";
+        } else {
+            document.getElementById(value_pointer).innerText += raw_value;
+        }
+
+        console.log(raw_value)
+}
+
+var submitBtn = document.getElementById("submit")
+submitBtn.addEventListener("click", calculate)
+    var clearBtn = document.getElementById("clear")
+    clearBtn.addEventListener("click", clear)
+
+function calculate(){
+    var value1 = parseInt(document.getElementById("value1").innerText);
+    var value2 = parseInt(document.getElementById("value2").innerText);
+    if(operator == '+'){
+
+        document.getElementById("result").innerText = value1 + value2;
+    }
+    else if(operator == '*'){
+        document.getElementById("result").innerText = value1 * value2;
+    }
+
+    //Nochmals initialisieren
+    value_pointer = "value1";
+
+}
+
+    function clear(){
+        document.getElementById("value1").innerText = "";
+        document.getElementById("value2").innerText = "";
+        document.getElementById("result").innerText = "";
+        document.getElementById("operator").innerText = "";
+    }
+});
+
+
